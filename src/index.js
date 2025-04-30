fea644bbe796647e8f15fe04528f2d69d69d9841
JC BUISSON <buisson@enseeiht.fr>
2025-04-30 13:58:20 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -8,16 +8,15 @@ program
   .description('A simple greeting CLI')
   .version('1.0.0');
 
+// Add a positional argument called "name"
 program
-  .option('-n, --name <string>', 'name to greet')
-  .option('-t, --times <number>', 'number of times to greet', '1');
+  .argument('<name>', 'name to greet')
+  .option('-t, --times <number>', 'number of times to greet', '1')
+  .action((name, options) => {
+      const times = parseInt(options.times);
+      for (let i = 0; i < times; i++) {
+      console.log(`Hello, ${name}!`);
+      }
+   })
 
-program.parse(process.argv);
-
-const options = program.opts();
-const name = options.name || 'World';
-const times = parseInt(options.times);
-
-for (let i = 0; i < times; i++) {
-  console.log(`Hello, ${name}!`);
-}
+program.parse()
