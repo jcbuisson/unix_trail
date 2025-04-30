ad70f5374596ede1c52ba14520c7498f4f3f9ff6
JC BUISSON <buisson@enseeiht.fr>
2025-04-30 13:55:13 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log('Unix trail!', process.argv)
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .name('greet')
+  .description('A simple greeting CLI')
+  .version('1.0.0');
+
+program
+  .option('-n, --name <string>', 'name to greet')
+  .option('-t, --times <number>', 'number of times to greet', '1');
+
+program.parse(process.argv);
+
+const options = program.opts();
+const name = options.name || 'World';
+const times = parseInt(options.times);
+
+for (let i = 0; i < times; i++) {
+  console.log(`Hello, ${name}!`);
+}
