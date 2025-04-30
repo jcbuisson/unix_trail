cacdf13636a68ee15f8fb50ed78c0c7d7650d3a0
JC BUISSON <buisson@enseeiht.fr>
2025-04-30 14:17:22 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-const program = new Command();
+import { Command } from 'commander'
+const program = new Command()
 
 program
-  .name('unix_trail')
-  .description('Fun activities to learn Unix commands')
-  .version('1.0.0');
+   .name('unix_trail')
+   .description("Fun activities to learn Unix commands")
+   .version('1.0.0')
 
-// Add a positional argument called "name"
 program.command('generate')
-   .description('Generate a new set of activities')
-   .argument('<string>', 'string used to make activities unique')
+   .description("Generate a new set of activities")
+   .argument('<string>', "string used to make activities unique")
    .action((string, options) => {
       console.log('string', string)
+      console.log('options', options)
+   })
+
+program.command('guide')
+   .description("Display a guide for the next step")
+   .action((string, options) => {
+      console.log('options', options)
+   })
+
+program.command('check')
+   .description("Check current step")
+   .action((string, options) => {
       console.log('options', options)
    })
 
