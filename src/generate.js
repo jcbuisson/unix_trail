a29fe8d27b37f3332a4f87caf3491990b560c5dd
JC BUISSON <buisson@enseeiht.fr>
2025-04-30 15:44:00 +0200

    WIP

--- src/generate.js
+++ src/generate.js
@@ -3,7 +3,7 @@ import { mkdir } from 'fs/promises'
 export async function generate(string) {
    try {
       console.log("Generate!!", string)
-      await mkdir('./unix_trail')
+      await mkdir('./.unix_trail')
    } catch(err) {
       console.log(err)
    }
