6c47bd5aa2f7fda896d2cc7563d5bd532f8fa573
JC BUISSON <buisson@enseeiht.fr>
2025-04-30 15:52:07 +0200

    WIP

--- src/generate.js
+++ src/generate.js
@@ -3,7 +3,7 @@ import { mkdir, exists } from 'fs/promises'
 export async function generate(string) {
    try {
       console.log("Generate!!", string)
-      const stats = await exists('./.unix_trailll')
+      const exists = await exists('./.unix_trailll')
       console.log('exists', exists)
       // await mkdir('./.unix_trail', { recursive: true }) // no error if directory already exists
    } catch(err) {
